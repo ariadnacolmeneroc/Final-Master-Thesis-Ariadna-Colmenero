e6379182a034a256041eebc9a3c9fec5014ccfb3
Ariadna Colmenero <100841697+ariadnacolmeneroc@users.noreply.github.com>
2022-09-11 16:45:05 +0200

    Update SNPEff_SNPSift.py

--- SNPEFF_SNPSift/SNPEff_SNPSift.py
+++ SNPEFF_SNPSift/SNPEff_SNPSift.py
@@ -42,9 +42,8 @@ import subprocess # Call python scripts from python.
 
 for f in glob.glob("*_filt.vcf"):
       avinput = f.replace("_filt.vcf", ".avinput")
-      final = f.replace("_filt.vcf", ".txt")
-      bashArguments = "perl /media/ramis/Elements/SCRIPTS/annovar/convert2annovar.pl -format vcf4 "+f+" -outfile "+avinput+" -includeinfo -withzyg"
+      bashArguments =  java -jar SnpSift.jar dbnsfp -v -db db/dbNSFP4.1a.txt.gz D4669_filt.vcf -m -a | 
+                       java -Xmx12g -jar SnpSift.jar annotate -info 'CLNDISDB,CLNDISDBINCL,CLNDN,CLNSIG,CLNREVSTAT' CLINVAR/clinvar.vcf.gz /dev/stdin -a |
+                       java -Xmx12g -jar snpEff.jar -canon GRCh37.75 > D4669_filt_ann_c.vcf
       subprocess.call(bashArguments, shell=True)
-      
-      bashArguments = "perl /media/ramis/Elements/SCRIPTS/annovar/table_annovar.pl "+avinput+" /media/ramis/Elements/annovar/humandb/ -buildver hg19 -out "+final+" -remove -protocol refGene,cytoBand,exac03,gnomad211_exome,gnomad211_genome,dbnsfp33a -operation g,r,f,f,f,f -nastring . -otherinfo -thread 8"
-      subprocess.call(bashArguments, shell=True)
+
